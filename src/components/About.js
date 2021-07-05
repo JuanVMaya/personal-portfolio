49f8736e02ad9e67ca18586c6b2e90df24a4b50e
theonlyouan <juanjovanegas21@gmail.com>
2021-07-04 22:58:55 -0400

    Layed out the "About" page with content

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,20 @@
 import thinker from '../assets/Thinker.svg'
 import classes from './About.module.css'
+import disciplines from '../assets/Disciplines.svg'
 
-const About=()=>{
-    return(
+const About = () => {
+    return (
         <section className={classes.landingPage}>
-            <div>
-                <p>Hello, I'm</p>
-                <h1>Juan Maya</h1>
-                <p>_____ Developer</p>
+            <div className={classes.description}>
+                <h1>About</h1>
+                <p className={classes.aboutMe}>
+                    Results-oriented college graduate with an Advanced Diploma in Electronics Engineering
+                    Technology (3.9 GPA), working in the Electronics Manufacturing Industry. Aiming to leverage
+                    academic experience, proven knowledge of various development disciplines and modern development
+                    tools to build the future of the internet.
+                </p>
             </div>
-            <img className={classes.thinkerImg} src={thinker} alt='A person beside a world of spinning geometric figures' />
+            <img className={classes.disciplines} src={disciplines} alt='A person beside a world of spinning geometric figures' />
         </section>
     );
 };
