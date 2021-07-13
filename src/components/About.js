74b2ae7e4ec6ece7cdd84b82e792b422a0cffe60
theonlyouan <juanjovanegas21@gmail.com>
2021-07-12 21:34:55 -0400

    Change styling on Landing page and About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
-import thinker from '../assets/Thinker.svg'
 import classes from './About.module.css'
 import disciplines from '../assets/Disciplines.svg'
+import backgroundDetail from '../assets/AboutBackground.svg'
 
 const About = () => {
     return (
         <section className={classes.landingPage}>
+            <h1 className={classes.sectionTitle}>About</h1>
             <div className={classes.description}>
-                <h1>About</h1>
                 <p className={classes.aboutMe}>
                     Results-oriented college graduate with an Advanced Diploma in Electronics Engineering
                     Technology (3.9 GPA), working in the Electronics Manufacturing Industry. Aiming to leverage
@@ -15,6 +15,7 @@ const About = () => {
                 </p>
             </div>
             <img className={classes.disciplines} src={disciplines} alt='A person beside a world of spinning geometric figures' />
+            <img className={classes.backgroundDetail} src={backgroundDetail} alt='A geometric backrgound representing a downhill landscape' />
         </section>
     );
 };
