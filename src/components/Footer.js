6123740a9b7ab8f64dc733951ce0c141d2899864
theonlyouan <juanjovanegas21@gmail.com>
2021-09-16 19:50:14 -0400

    Configured Social Media Icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import classes from "./Footer.module.css";
 import Button from "./UI/Button";
 import { SocialIcon } from 'react-social-icons';
+import resume from "../assets/JuanVanegas-Resume.pdf";
 
 const Footer = () => {
     return (
@@ -14,20 +15,20 @@ const Footer = () => {
                 <div className={classes.rightContainer}>
                     <div className={classes.socialLogosContainer} >
                         <div className={classes.logoContainer}>
-                            <SocialIcon url="mailto:juanjovanegas21@gmail.com" bgColor="#c7c8c9" className={classes.socialIcon}/>
+                            <SocialIcon url="mailto:juanjovanegas21@gmail.com" bgColor="#c7c8c9" style={{height:35, width:35}} className={classes.socialIcon} />
                         </div>
                         <div className={classes.logoContainer}>
-                            <SocialIcon url="https://twitter.com/JuanVMaya" bgColor="#c7c8c9" className={classes.socialIcon}/>
+                            <SocialIcon url="https://twitter.com/JuanVMaya" bgColor="#c7c8c9" style={{height:35, width:35}} className={classes.socialIcon} />
                         </div>
                         <div className={classes.logoContainer}>
-                            <SocialIcon url="https://github.com/jvane1" bgColor="#c7c8c9" className={classes.socialIcon}/>
+                            <SocialIcon url="https://github.com/jvane1" bgColor="#c7c8c9" style={{height:35, width:35}} className={classes.socialIcon} />
                         </div>
                         <div className={classes.logoContainer}>
-                            <SocialIcon url="https://www.linkedin.com/in/juan-jose-vanegas-maya/" bgColor="#c7c8c9" className={classes.socialIcon}/>
+                            <SocialIcon url="https://www.linkedin.com/in/juan-jose-vanegas-maya/" bgColor="#c7c8c9" style={{height:35, width:35}} className={classes.socialIcon} />
                         </div>
-                        
                     </div>
-                    <Button className={classes.downloadResume}>Download Resume</Button>
+                    
+                    <a href={resume} target="_blank" rel="noopener noreferrer" style ={{textDecoration:"None"}}><Button>Download Resume</Button></a>
                 </div>
             </div>
             <div className={classes.copyrightSection}>© Copyright 2021</div>
