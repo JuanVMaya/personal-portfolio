8826b253af996fee7f215ae2b47064920c187793
theonlyouan <juanjovanegas21@gmail.com>
2021-09-23 21:13:00 -0400

    Updated Project Card Styling and Project content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
-import Card from './UI/Card';
+import ProjectCard from './UI/ProjectCard';
 import classes from './Projects.module.css';
-import Thinker from '../assets/Thinker.svg';
 import Button from './UI/Button';
 import backgroundDetail from '../assets/AboutBackground2.svg';
 
@@ -8,21 +7,23 @@ const ProjectData = [
     {
         id: 'm1',
         title: 'Personal Portfolio',
-        thumbnail: require('../assets/Thinker.svg'),
+        thumbnail: require("../assets/PersonalPortfolio.png").default,
         altText: 'A person thinking while standing',
-        description: 'This website is a compilation of materials that exemplifies my beliefs, skills, qualifications, education, training and experiences. It provides insight into your personality and work ethic.This is a representation of self-taught all-in-one project. It includes technolgies and tools such such as HTML, CSS, React, Firebase and GitHub.',
+        description: 'This website is a compilation of materials that exemplifies my beliefs, skills, qualifications, education, training and experiences. It provides insight into my personality and work ethic.This is a representation of self-taught all-in-one project. It includes technolgies and tools such such as Figma, HTML, CSS, React, Firebase and GitHub.',
+        learnMoreLink: 'https://github.com/jvane1/personal-portfolio',
+    },
+    {
+        id: 'm2',
+        title: 'Arithmetic Arranger',
+        thumbnail: require('../assets/ArithmeticArranger.png').default,
+        altText: 'A person thinking while standing',
+        description: 'Students in primary school often arrange arithmetic problems vertically to make them easier to solve. Function that receives a list of strings that are arithmetic problems and returns the problems arranged vertically and side-by-side. The function should optionally take a second argument. When the second argument is set to True, the answers should be displayed.',
+        learnMoreLink: 'https://replit.com/@jvane1/arithmetic-formatter#arithmetic_arranger.py',
     },
     {
         id: 'm2',
         title: 'Personal Portfolio',
-        thumbnail: require('../assets/Thinker.svg'),
-        altText: 'A person thinking while standing',
-        description: 'This website is a compilation of materials that exemplifies my beliefs, skills, qualifications, education, training and experiences. It provides insight into your personality and work ethic.This is a representation of self-taught all-in-one project. It includes technolgies and tools such such as HTML, CSS, React, Firebase and GitHub.',
-    },
-    {
-        id: 'm2',
-        title: 'Personal Portfolio',
-        thumbnail: require('../assets/Thinker.svg'),
+        thumbnail: require('../assets/Thinker.svg').default,
         altText: 'A person thinking while standing',
         description: 'This website is a compilation of materials that exemplifies my beliefs, skills, qualifications, education, training and experiences. It provides insight into your personality and work ethic.This is a representation of self-taught all-in-one project. It includes technolgies and tools such such as HTML, CSS, React, Firebase and GitHub.',
     },
@@ -31,12 +32,12 @@ const ProjectData = [
 
 const Projects = props => {
     const projectsList = ProjectData.map((project) => (
-        <Card id={project.id} className={classes.card}>
+        <ProjectCard key={project.id} className={classes.card}>
             <h3>{project.title}</h3>
-            <img className={classes.thumbnail} src={Thinker} alt={project.altText} />
+            <img className={classes.thumbnail} src={project.thumbnail} alt={project.altText} />
             <p className={classes.projectDescription}>{project.description} </p>
-            <div className={classes.learnMoreContainer}><Button>Learn More</Button></div>
-        </Card>
+            <div className={classes.learnMoreContainer}><a href={project.learnMoreLink} target="_blank"><Button>Learn More</Button></a></div>
+        </ProjectCard>
     ));
     return (
         <section className={classes.projectsPage}>
